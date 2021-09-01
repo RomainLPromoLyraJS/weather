4161076addbf870d493e9db0d1966c0e816e10c1
Romain Lafosse <romain75l@gmail.com>
2021-09-01 17:10:00 +0200

    Start for the weather application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// -- React Imports
+import React from 'react';
+
+// - Local Imports
+import './app.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>je suis dans App.</p>
     </div>
   );
 }
