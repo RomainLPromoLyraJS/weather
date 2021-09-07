c836658d55628324d840081484418c39d8e97c23
Romain Lafosse <romain75l@gmail.com>
2021-09-07 15:01:21 +0200

    Static datas and CSS added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import React from 'react';
 // - Local Imports
 import './app.css';
 
-const api = {
-  key: process.env.API_WEATHER_KEY,
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+// const api = {
+//   key: process.env.API_WEATHER_KEY,
+//   base: "https://api.openweathermap.org/data/2.5/"
+// }
 
 function App() {
+
+  const dateBuilder = (day) => {
+    let months = ["January", "February", "March", "April", "Mai", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let oneDay = days[day.getDay()];
+    let date = day.getDate();
+    let month = months[day.getMonth()];
+    let year = day.getFullYear();
+
+    return `${oneDay} ${date} ${month} ${year}`;
+  }
+
   return (
     <div className="app">
       <main>
@@ -19,6 +32,14 @@ function App() {
             type="text" 
             placeholder="Search..." 
           />
+        </div>
+        <div className="location-box">
+          <div className="location">Annecy, FR</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15°C</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
