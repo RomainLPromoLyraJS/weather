2d0cb11a3997b1c932ce2fc7b865733b1d7032c8
Romain Lafosse <romain75l@gmail.com>
2021-09-07 12:26:26 +0200

    Creation of the CSS for the app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import React from 'react';
 // - Local Imports
 import './app.css';
 
+const api = {
+  key: process.env.API_WEATHER_KEY,
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
-    <div className="App">
-      <p>je suis dans App.</p>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+            className="search-bar" 
+            type="text" 
+            placeholder="Search..." 
+          />
+        </div>
+      </main>
     </div>
   );
 }
